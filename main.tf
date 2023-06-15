a91f7a98ceac34ec2034ff26e0fe4576a83e24fd
Jéssica <jessica@example.com>
2023-06-15 15:46:40 -0300

    Adicionando user_data

--- main.tf
+++ main.tf
@@ -17,7 +17,13 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0c65adc9a5c1b5d7c"
   instance_type = "t2.micro"
   key_name = "iac-alura"
+  user_data = <<-EOF
+                 #!/bin/bash
+                 cd /home/ubuntu
+                 echo"<h1>Feito com Terraform</h1>" > index.html
+                 nohup busybox httpd -f -p 8080 & 
+                 EOF   
   tags = {
-    Name = "IaC Formação DevOps Alura"
+    Name = "Teste AWS"
   }
 }
